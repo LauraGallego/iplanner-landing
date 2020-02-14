1214e2998e12b2af8375942a5a25f159504bd4a0
LauraGallego <caroline.grafico@gmail.com>
2020-02-14 13:21:11 -0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,15 @@
+$("#menu-iplanner").click(() => (mover($("#some").offset().top, 1000)));
+$("#animacion-beneficio").click(() => (mover($("#bene").offset().top, 1000)));
+$("#animacion-como-funciona").click(() => (mover($("#funcion").offset().top, 1000)));
+
+function mover(position, speed) {
+    $("html, body").animate({
+        scrollTop: position,
+    }, speed);
+}
+
+$(".computador").animate({"margin-right": 0}, 1000)
+
+$("#descarga").click(()=> {
+window.open("manual.pdf","_blank")
+});
