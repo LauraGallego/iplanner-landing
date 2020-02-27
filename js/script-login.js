46a78d3bd37c5a38e8d8a73e6a1ce8f8b3918657
Osman Beltran Murcia <osman.beltran@fouriering.com>
2020-02-27 13:45:53 -0500

    - Se agrego la encriptacion de los datos - Se corrigieron algunos errores en el nombramiento de las variables - Se agrego la validacion de usuario y contraseña
    * Queda pendiente modificar el endpoint a la ip publica del servidor final*

--- js/script-login.js
+++ js/script-login.js
@@ -1,22 +1,68 @@
+/*
+       -------------------------------- Comentario de modificación -------------------------------- 
+    * Se agrego 
+        - La encriptacion del lado del cliente 
+        - El manejo de respuestas enviadas por el servidor de IPlanner
+*/
+const route = "http://localhost:9806"
 $("#boton-iniciar").click(function(event) {
-    $.post('https://cors-anywhere.herokuapp.com/http://54.173.133.231:9806/database/login', { email: $("#email").val(), contrasena: $("#contrasena").val(), },
-        function(data, textStatus, xhr) {
-            if (data.response == 1) {
-                alert("ingreso-al-sistema")
-            } else {
-                $(".alerta").toggle("puff", { percent: 20 }, 100)
+    if (verifyInputs()) return;
+
+    var bf = new Blowfish("F%0uR1&_3rING$");
+
+    $.post(route + '/access/login', { user: $("#user").val(), password: bf.base64Encode(bf.encrypt($("#password").val())), },
+        (response) => {
+            console.log(response)
+            /*
+            
+             * Codigo 200:     [Success]         Autenticacion realizada con exito
+            
+             * Codigo 401:     [Unathorized]     El usuario no existe en los registros de leonisa
+            
+             * Codigo 403:     [Forbidden]       El usuario existe en Leonisa pero no tiene acceso a la plataforma    
+            
+            */
+            switch (response.status) {
+                case 200:
+                    alert("Usuario autenticado correctamente")
+                    location.replace("http://localhost:4200/indicators/campaign")
+                    break;
+                case 401:
+                    showAlert("Usuario o contraseña <b>incorrectos</b>")
+                    break;
+                case 403:
+                    showAlert("Acceso al sistema <b>prohibido</b>")
+                    break;
             }
         });
 });
 
-$(".manual-usuario").click(function(event) {
-    window.open("manual.pdf")
-});
 
-$(".logo-direccion").click(function(event) {
-	window.location="index.html"
-});
 
-window.onload=function(){
-	$(".cover").fadeOut(800)
+$(".manual-usuario").click(() => (window.open("manual.pdf")));
+
+$(".logo-direccion").click(() => (window.location = "index.html"));
+
+window.onload = () => ($(".cover").fadeOut(800))
+
+/*
+ * Se realizo una abstraccion de la funcion de mostrar errores
+ */
+let showAlert = msg => {
+    $(".alerta").html(msg)
+    $(".alerta").removeClass('hide')
+}
+
+/*
+ * Se agrego una verificacion extra de campos vacios
+ */
+let verifyInputs = () => {
+    if ($("#user").val() === "" && $("#password").val() === "")
+        showAlert("Tienes que llenar los <b>datos</b> de ingreso")
+    else if ($("#user").val() === "")
+        showAlert("El campo <b>usuario</b> no puede estar vacio")
+    else if ($("#password").val() === "")
+        showAlert("El campo <b>contraseña</b> no puede estar vacio")
+
+    return ($("#user").val() === "" || $("#password").val() === "")
 }
